ec7aa7e732329ef5f54aaebefafcc8bede6b035c
LewandowskiJakub16 <r16lewandowski@gmail.com>
2023-03-08 12:42:24 +0100

    Zajecia nr2

---
+++ 3_dolaczanie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Poczatek strony</h4>
+    <?php
+    //include, require_once, require, include_once
+    include 'skrypty/lista.php';
+    require_once 'skrypty/lista.php';
+    require 'skrypty/lista.php';
+    ?>
+    <h4>Koniec strony</h4>
+</body>
+</html>
